71be32fc69da789e63211b700a01a40c6bf7cb59
Morten Hansen <morten@winterop.com>
2024-08-22 10:36:01 +0200

    extract id for insert/update

--- examples/java/d2_camel_cdc/src/main/java/com/example/demo/routes/PgReplicationSlotRoute.java
+++ examples/java/d2_camel_cdc/src/main/java/com/example/demo/routes/PgReplicationSlotRoute.java
@@ -1,31 +1,74 @@
 package com.example.demo.routes;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import lombok.Data;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
+enum ChaneType {
+  CREATE,
+  UPDATE
+}
+
 @Component
 public class PgReplicationSlotRoute extends RouteBuilder {
 
-    @Override
-    public void configure() throws Exception {
-        from("pg-replication-slot://localhost:15432/dhis/test_slot2:wal2json"
+  @Override
+  public void configure() throws Exception {
+    from("pg-replication-slot://localhost:15432/dhis/test_slot4:wal2json"
             + "?user=dhis"
             + "&password=dhis"
             + "&slotOptions.include-xids=true"
             + "&slotOptions.add-tables=public.organisationunit")
-            .routeId("replication-slot")
-            .filter().jsonpath("$.change[?(@.length() > 0)]")
-            // .log("Received event: ${body}") // debug
-            .unmarshal(new JacksonDataFormat(Changes.class))
-            .process(exchange -> {
-              Changes payload = exchange.getIn().getBody(Changes.class);
-              System.out.println("Deserialized Payload: " + payload);
-            });
+        .routeId("replication-slot")
+        .filter()
+        .jsonpath("$.change[?(@.length() > 0)]")
+        .unmarshal(new JacksonDataFormat(Changes.class))
+        .process(
+            exchange -> {
+              Changes changes = exchange.getIn().getBody(Changes.class);
+              exchange.getIn().setBody(findChangeIds(changes));
+            })
+        .log("${body}");
+  }
+
+  private List<ChangeInfo> findChangeIds(Changes changes) {
+    List<ChangeInfo> changeInfos = new ArrayList<>();
+
+    for (Change change : changes.getChange()) {
+      ChangeInfo changeInfo = new ChangeInfo();
+
+      if (change.getKind().equals("insert")) {
+        changeInfo.setType(ChaneType.CREATE);
+      } else if (change.getKind().equals("update")) {
+        changeInfo.setType(ChaneType.UPDATE);
+      } else {
+        continue;
+      }
+
+      int idIndex =
+          change.getColumnnames().stream()
+              .filter("uid"::equals)
+              .findFirst()
+              .map(change.getColumnnames()::indexOf)
+              .orElse(-1);
+
+      if (idIndex != -1) {
+        changeInfo.setId(change.getColumnvalues().get(idIndex));
+        changeInfos.add(changeInfo);
+      }
     }
+
+    return changeInfos;
+  }
+}
+
+@Data
+class ChangeInfo {
+  private ChaneType type;
+  private String id;
 }
 
 @Data
@@ -33,17 +76,17 @@ class Change {
   private String kind;
   private String schema;
   private String table;
-  private List<String> columnnames;
-  private List<String> columntypes;
-  private List<String> columnvalues;
+  private List<String> columnnames = new ArrayList<>();
+  private List<String> columntypes = new ArrayList<>();
+  private List<String> columnvalues = new ArrayList<>();
   private OldKeys oldkeys;
 }
 
 @Data
 class OldKeys {
-  private List<String> keynames;
-  private List<String> keytypes;
-  private List<Object> keyvalues;
+  private List<String> keynames = new ArrayList<>();
+  private List<String> keytypes = new ArrayList<>();
+  private List<Object> keyvalues = new ArrayList<>();
 }
 
 @Data
